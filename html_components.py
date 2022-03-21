cea0f4bcd19a3a5cc54f075ec0264cb263e9d159
Devamitta <rusdevamitta@gmail.com>
2022-03-21 14:29:56 +0800

    consice

--- html_components.py
+++ html_components.py
@@ -32,60 +32,38 @@ def render_word_meaning(w: DpdWord) -> RenderResult:
 
 
     else:
-        html_string += f"""<div class="content_dps"><p>{w.pos}"""
-        text_concise += f"{w.pali}. {w.pos}."
+        html_string += f"""<div class="content_dps"><p>"""
 
-        if w.fin == "n":
-            html_string += f""" [{w.fin}]"""
-            text_concise += f""" [{w.fin}]"""
+        if w.pos != "":
+            html_string += f"""{w.pos}."""
+            text_concise += f"{w.pali}. {w.pos}."
 
-        if w.fin == "nn":
-            html_string += f""" [{w.fin}]"""
-            text_concise += f""" [{w.fin}]"""
+        #     if w.fin == "n":
+        #         html_string += f""" [{w.fin}]."""
+        #         text_concise += f""" [{w.fin}]."""
 
-        if w.fin == "np":
-            html_string += f""" [{w.fin}]"""
-            text_concise += f""" [{w.fin}]"""
+        #     if w.fin == "nn":
+        #         html_string += f""" [{w.fin}]."""
+        #         text_concise += f""" [{w.fin}]."""
 
-        if w.fin == "ns":
-            html_string += f""" [{w.fin}]"""
-            text_concise += f""" [{w.fin}]"""
+        #     if w.fin == "np":
+        #         html_string += f""" [{w.fin}]."""
+        #         text_concise += f""" [{w.fin}]."""
 
-        if w.fin == "pn":
-            html_string += f""" [{w.fin}]"""
-            text_concise += f""" [{w.fin}]"""
+        #     if w.fin == "ns":
+        #         html_string += f""" [{w.fin}]."""
+        #         text_concise += f""" [{w.fin}]."""
 
-        if w.fin == "s":
-            html_string += f""" [{w.fin}]"""
-            text_concise += f""" [{w.fin}]"""
+        #     if w.fin == "pn":
+        #         html_string += f""" [{w.fin}]."""
+        #         text_concise += f""" [{w.fin}]."""
 
-        # if w.case != "":
-        #     html_string += f""" ({w.case})"""
-        #     text_concise += f""" ({w.case})"""
+        #     if w.fin == "s":
+        #         html_string += f""" [{w.fin}]."""
+        #         text_concise += f""" [{w.fin}]."""
 
-        html_string += f""". <b>{w.russian}</b>"""
-        text_concise += f""". {w.russian}"""
-
-        # if w.base == "":
-        #     construction_simple = re.sub(r" \[.+\] \+", "", w.construction)
-        #     construction_simple = re.sub("> .+? ", "", construction_simple)
-        #     construction_simple = re.sub("<br/>.+", "", construction_simple)
-        #     if construction_simple != "":
-        #         html_string += f""". [{construction_simple}]"""
-        #         text_concise += f""". [{construction_simple}]"""
-
-        # if w.base != "":
-        #     family_plus = re.sub(" ", " + ", w.family)
-        #     construction_oneline = re.sub("<br/>.+", "", w.construction)
-        #     construction_truncated = re.sub(r"(.+)(\+ .{1,7}$)", "\\2", construction_oneline)
-        #     if re.match("^na ", w.construction):
-        #         construction_na = re.sub("^(na )(.+)$", "\\1 + ", w.construction)
-        #     else:
-        #         construction_na = ""
-
-        #     construction_reconstructed = f"{construction_na}{family_plus} + {w.root_sign} {construction_truncated}"
-        #     html_string += f""" [{construction_reconstructed}]"""
-        #     text_concise += f""" [{construction_reconstructed}]"""
+        html_string += f""" <b>{w.russian}</b>"""
+        text_concise += f""" {w.russian}"""
 
         html_string += f"""</p></div>"""
 
