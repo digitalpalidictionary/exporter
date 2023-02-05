7bfe3e855f2c82d3c6c5fe76d0b430ec7b6ccb0f
bdhrs <bodhirasa@gmail.com>
2023-02-05 14:52:46 +0530

    construciton summary

--- html_components.py
+++ html_components.py
@@ -48,27 +48,49 @@ def render_word_meaning(w: DpdWord) -> RenderResult:
         if w.base == "":
             construction_simple = re.sub("<br/>.+$", "", w.construction) # remove line2
             construction_simple = re.sub(r" \[.+\] \+", "", construction_simple) # remove [insertions]
-            construction_simple = re.sub("> .+? ", "", construction_simple) # remove phonetic changes
-            construction_simple = re.sub(" > .+?$", "", construction_simple) # remove phonetic changes at end
+            construction_simple = re.sub("> .[^ ]+? ", "", construction_simple) # remove phonetic changes
+            construction_simple = re.sub(" > .[^ ]+?$", "", construction_simple) # remove phonetic changes at end
 
             if construction_simple != "":
                 html_string += f""" [{construction_simple}]"""
                 text_concise += f""" [{construction_simple}]"""
 
-        if w.base != "":
+        if w.base != "" and w.pos != "fut":
             base_clean = re.sub(" \\(.+\\)$", "", w.base)
             base_clean = re.sub("(.+ )(.+?$)", "\\2", base_clean)
             family_plus = re.sub(" ", " + ", w.family)
             construction_oneline = re.sub("<br/>.+", "", w.construction)
-            construction_truncated = re.sub(" > .[^ ]+", "", w.construction)
+            construction_truncated = re.sub(" > .[^ ]+", "", construction_oneline)
             construction_truncated = re.sub(f".*{base_clean}", "", construction_truncated)
 
             if re.match("^na ", w.construction):
                 construction_na = re.sub("^(na )(.+)$", "\\1+ ", w.construction)
+                construction_truncated = re.sub(r"na > a|na > an|na > ana", "", construction_truncated)
             else:
                 construction_na = ""
 
             construction_reconstructed = f"{construction_na}{family_plus} + {w.root_sign}{construction_truncated}"
+            html_string += f""" [{construction_reconstructed}]"""
+            text_concise += f""" [{construction_reconstructed}]"""
+        
+        if w.base != "" and w.pos == "fut":
+            base = re.sub(" > .+ \\(.+\\)$", "", w.base)   # remove > base and end brackets
+            base = re.sub("√.[^ ]* \\+ ", "", base)
+            family_prefix = re.sub(" ", " + ", w.family)
+            construction_truncated = re.sub(
+                " > .[^ ]+", "", w.construction)  # remove phonetic changes
+            construction_truncated = re.sub(
+                r"(.+)( \+ .*$)", "\\2", construction_truncated)
+
+            if re.match("^na ", w.construction):
+                construction_na = re.sub(
+                    "^(na )(.+)$", "\\1+ ", w.construction)
+                construction_truncated = re.sub(
+                    r"na > a|na > an|na > ana", "", construction_truncated)
+            else:
+                construction_na = ""
+
+            construction_reconstructed = f"{construction_na}{family_prefix} + {base}{construction_truncated}"
             html_string += f""" [{construction_reconstructed}]"""
             text_concise += f""" [{construction_reconstructed}]"""
         
